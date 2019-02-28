99f7cd1dd318f2dc3a21165d8d667d33738941eb
Shea Stewart <sheastewart@sheasus.local>
2019-02-27 19:45:25 -0800

    update test script

--- sysdig/test.py
+++ sysdig/test.py
@@ -2,19 +2,24 @@
 
 # import client
 import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), '..'))
 from sdcclient import SdcClient
 
 # Input Sysdig API token
-api_token = os.environ['SYSDIG_API_TOKEN']
+sdc_token = os.environ['SYSDIG_API_TOKEN']
 
 # Instantiate the Sysdig client
 
 sdclient = SdcClient(sdc_token)
 
+team_name = sys.argv[1]
+
+
 print('Now trying to create a team with name:', team_name)
 ok, res = sdclient.create_team(team_name)
 if not ok:
     print('Team creation failed:', res, '. Exiting.')
-    sys.exit(1)
 else:
     print('Team creation succeeded.', res)
+
